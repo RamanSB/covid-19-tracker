82e32a596f7fa2431f1384f6220d2fb871244b39
RamanSB <ramansb100@gmail.com>
2021-03-28 14:26:02 +0100

    Removed API Keys on remote repository

--- src/api/credentials.js
+++
@@ -1,9 +0,0 @@
-const apiKey = "1d5b522e84msh50a3c54b94f14a9p11568bjsn92dce6a6a3e9";
-const apiHost = "covid-19-data.p.rapidapi.com";
-
-let apiCredentials = {
-  apiHost: apiHost,
-  apiKey: apiKey
-}
-
-export default apiCredentials;
